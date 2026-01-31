eef13db56f0da900907d495c672936ebb3c89681
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-01-31 21:35:21 +0100

    Add Component

---
+++ src/Component.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Slendium\Compositor;
+
+/**
+ * Generic container for composable contents, which may include other components.
+ *
+ * @since 1.0
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+interface Component { }
