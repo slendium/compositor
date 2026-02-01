cfb14a9a7d8f78ab12c79a160f2e3be9917d55fa
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-01 16:48:28 +0100

    Add CompositorAdapter

---
+++ src/Base/CompositorAdapter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Slendium\Compositor\Base;
+
+use Slendium\Localization\Localizable;
+
+use Slendium\Compositor\Component;
+use Slendium\Compositor\Replaceable;
+
+/**
+ * Defines conversions from components and their composed parts to an output type.
+ *
+ * @since 1.0
+ * @template TComponent of Component
+ * @template TPart
+ * @template TOutput
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+interface CompositorAdapter {
+
+	/**
+	 * @since 1.0
+	 * @param TComponent $component
+	 * @return iterable<Replaceable|TComponent|TPart|Localizable<Replaceable|TComponent|TPart>>
+	 */
+	public function getDescendants(Component $component): iterable;
+
+	/**
+	 * @since 1.0
+	 * @param TPart $part
+	 * @return iterable<TOutput>
+	 */
+	public function generateOutput(mixed $part): iterable;
+
+}
