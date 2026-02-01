bd4476d93a6a07f2cab31623a0d5e8718447738b
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-01 16:47:27 +0100

    Add Replaceable

---
+++ src/Replaceable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Slendium\Compositor;
+
+/**
+ * A composition part that the compositor may replace by another part.
+ *
+ * @since 1.0
+ * @author C. Fahner
+ * @copyright Slendium 2026
+ */
+interface Replaceable { }
