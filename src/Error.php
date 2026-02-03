038cae3e356c2dbbe49d066fcbbc62f1d72d114e
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-02-03 19:53:17 +0100

    Add Error (interface)

---
+++ src/Error.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Slendium\Compositor;
+
+/**
+ * A composition error.
+ *
+ * @since 1.0
+ * @author
+ * @copyright Slendium 2026
+ */
+interface Error {
+
+	/** @since 1.0 */
+	public string $message { get; }
+
+}
